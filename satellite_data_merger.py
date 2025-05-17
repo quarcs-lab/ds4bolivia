3b07c8634337957495c6156c090bdf6f22dd09fb
Hendrix Peralta <hendrixperalta@gmail.com>
2025-05-17 12:27:51 +0900

    Merged the Air temperature min dataset 2012-2017

--- satellite_data_merger.py
+++ satellite_data_merger.py
@@ -7,10 +7,21 @@ from datetime import datetime
 db = pd.read_csv("../project2021o/data/GeoDS4Bolivia_v20250512.csv") # 139 variables
 desc = pd.read_csv("../project2021o/data/Definitions_GeoDS4Bolivia_v20250512.csv") 
 # %%
+
+# Air temperature
 airTempMean_url = "https://raw.githubusercontent.com/HendrixPeralta/bol_hdi_prediction/refs/heads/main/data/satellite/collab_satellite_data/air_tem_mean_2012-17.csv"
+
+airTempMin_url = "https://raw.githubusercontent.com/HendrixPeralta/bol_hdi_prediction/refs/heads/main/data/satellite/collab_satellite_data/air_temp_min_2012-2017.csv"
+
+# airTempMax_url = "https://raw.githubusercontent.com/HendrixPeralta/bol_hdi_prediction/refs/heads/main/data/satellite/collab_satellite_data/air_temp_max_2012-2017.csv"
 
 airTempMean = pd.read_csv(airTempMean_url)
 db = db.merge(airTempMean, left_on="asdf_id", right_on="id")
+
+airTempMin = pd.read_csv(airTempMin_url)
+db = db.merge(airTempMin, left_on="asdf_id", right_on="id")
+
+# airTemp
 
 # %%
 
