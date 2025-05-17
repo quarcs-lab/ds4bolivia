c993d121e96381a541d82ac38c3338db06fdb42a
Hendrix Peralta <hendrixperalta@gmail.com>
2025-05-17 13:45:10 +0900

    Added the variable name and label information for the Air temperature variables

--- satellite_data_merger.py
+++ satellite_data_merger.py
@@ -10,12 +10,24 @@ desc = pd.read_csv("../project2021o/data/Definitions_GeoDS4Bolivia_v20250512.csv
 # %%
 
 # Air temperature
+
+#Adding new values to the dataset
 airTempMean_url = "https://raw.githubusercontent.com/HendrixPeralta/bol_hdi_prediction/refs/heads/main/data/satellite/collab_satellite_data/air_tem_mean_2012-17.csv"
 
 airTempMean = pd.read_csv(airTempMean_url)
 db = db.merge(airTempMean, left_on="asdf_id", right_on="id", right_index=False)
 db.drop(columns="id", inplace=True)
+# %%
+vNames = airTempMean.columns[~airTempMean.columns.str.contains("id")].to_list()
 
+# Creating variable names and labels
+vLabel = []
+years = range(2012,2018)
+for year in years:
+    vLabel.append(f"Mean Air Temperature in Celcius in {year}")
+
+rows = pd.DataFrame({"varname":vNames, "varlabel":vLabel})
+desc = pd.concat([desc, rows], ignore_index=True)
 # %%
 
 airTempMin_url = "https://raw.githubusercontent.com/HendrixPeralta/bol_hdi_prediction/refs/heads/main/data/satellite/collab_satellite_data/air_temp_min_2012-2017.csv"
@@ -24,6 +36,18 @@ airTempMin = pd.read_csv(airTempMin_url)
 db = db.merge(airTempMin, left_on="asdf_id", right_on="id")
 db.drop(columns="id", inplace=True)
 
+
+# %%
+
+vNames = airTempMin.columns[~airTempMin.columns.str.contains("id")].tolist()
+
+vLabel = []
+for year in years:
+    vLabel.append(f"Minimum Air Temperature in Celcius in {year}")
+    
+rows = pd.DataFrame({"varname":vNames, "varlabel":vLabel})
+desc = pd.concat([desc,rows], ignore_index=True)
+
 # %%
 airTempMax_url = "https://raw.githubusercontent.com/HendrixPeralta/bol_hdi_prediction/refs/heads/main/data/satellite/collab_satellite_data/air_temp_max_2012-2017.csv"
 
@@ -31,11 +55,27 @@ airTempMax = pd.read_csv(airTempMax_url)
 db = db.merge(airTempMax, left_on="asdf_id", right_on="id")
 db.drop(columns="id", inplace=True)
 
+
+# %%
+
+vNames = airTempMax.columns[~airTempMax.columns.str.contains("id")].tolist()
+
+vLabel = []
+for year in years:
+    vLabel.append(f"Maximum Air Temperature in Celcius in {year}")
+    
+rows = pd.DataFrame({"varname":vNames, "varlabel":vLabel})
+desc = pd.concat([desc,rows], ignore_index=True)
+
+
 # %%
 
 # Exporting final version
 today = datetime.now().strftime("%Y%m%d")
-filename = f"GeoDS4Bolivia_v{today}.csv"
-db.to_csv(f"../project2021o/data/{filename}")
 
+db_filename = f"GeoDS4Bolivia_v{today}.csv"
+db.to_csv(f"../project2021o/data/{db_filename}")
+
+desc_filename = f"Definitions_GeoDS4Bolivia_v{today}.csv"
+desc.to_csv(f"../project2021o/data/{desc_filename}")
 # %%
